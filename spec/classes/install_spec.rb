83ed0deb9cd1950e60b6ba56bc8eeffe5feebc05
Scott Brimhall <scott@brimhallmail.com>
2018-02-09 00:00:10 -0800

    Add rspec test for resolvconf package on debian.

--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -5,7 +5,12 @@ describe 'resolvconf::install' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
-      it { is_expected.to compile }
+      it { is_expected.to compile.with_all_deps }
+
+      case os_facts[:osfamily]
+      when 'Debian'
+        it { is_expected.to contain_package('resolvconf') }
+      end
     end
   end
 end
